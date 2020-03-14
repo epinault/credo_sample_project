bc37ab8ea26a70d972ceef1da56641777bde8b67
René Föhring <rene.foehring@5minds.de>
2020-03-14 11:23:38 +0100

    Add consistency check as custom check

--- .credo.exs
+++ .credo.exs
@@ -2,10 +2,11 @@
   configs: [
     %{
       name: "default",
-      requires: ["./lib/my_project/checks/my_iex_pry.ex"],
+      requires: ["./lib/my_project/checks/**/*.ex"],
       checks: [
         {Credo.Check.Warning.IExPry, false},
-        {MyProject.Checks.MyIExPry, [excluded: ["lib/my_project.ex"]]}
+        {MyProject.Checks.MyIExPry, [excluded: ["lib/my_project.ex"]]},
+        {MyProject.Checks.SpaceInParentheses, []}
       ],
       plugins: [
         {CredoDemoPlugin, []}
